186c1b9c89eeeec3694abdea86b201a2fd265fe8
usedfuturedigital <alanc909@gmail.com>
2017-05-28 20:06:40 +0100

    :tada: added message components1

---
+++ Message-Component/main.js
@@ -0,0 +1,32 @@
+//vue component
+//slot allow user to type bettween tags
+Vue.component('message', {
+	props: ['title', 'body'],
+	data() {
+		return {
+			isVisible: true
+		};
+	},
+
+	template: `<article class="message" v-show="isVisible">
+	<div class="message-header">
+		{{title}}
+		<button class="delete" @click="hideModal"></button>
+	</div>
+	<div class="message-body">
+		{{body}}
+	</div>
+</article>`
+,
+
+methods:{
+	hideModal() {
+		this.isVisible= false;
+	}
+}
+
+});
+
+new Vue({
+	el: '#root'
+});
